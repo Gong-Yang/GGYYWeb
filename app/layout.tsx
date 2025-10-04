12cc20c7977bb37433b06d63a9795ea8350fb280
Gong.Yang <chaiyi.fun@qq.com>
2025-10-04 18:47:07 +0800

    页面埋点

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
 import "styles/tailwind.css"
+import Script from "next/script"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {
+          <Script
+            id="baidu-tongji"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                var _hmt = _hmt || [];
+                (function() {
+                  var hm = document.createElement("script");
+                  hm.src = "https://hm.baidu.com/hm.js?8321bd2330a998d38ae818a752a803f8";
+                  var s = document.getElementsByTagName("script")[0];
+                  s.parentNode.insertBefore(hm, s);
+                })();
+              `,
+            }}
+          />
+        }
+      </head>
       <body>{children}</body>
     </html>
   )
